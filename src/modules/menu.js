9e5af6c23af134861ce03d930c5a42d7b4ea2152
Vladimir Tkachenko <geletar@gmail.com>
2023-02-28 18:47:08 +0400

    lesson22

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,21 +1,28 @@
 const toggleMenu = () => {
   const menu = document.querySelector('menu');
-  const scrollBtn = document.body.querySelector('a');
   const body = document.querySelector('body');
+  const scrollBtn = body.querySelector('a');
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
   body.addEventListener('click', (e) => {
-    if (e.target.closest('.menu') || e.target.classList.contains('close-btn')) {
+    console.log(e.target.closest('.active-menu'));
+    console.log(e.target === menu);
+
+    if ((e.target.closest('.menu') && !e.target.closest('.active-menu'))
+      || e.target.classList.contains('close-btn')) {
       handleMenu();
+      return;
     }
-    if (e.target.closest('.active-menu')) {
+    if (e.target.closest('.active-menu')
+      && !e.target.classList.contains('active-menu')) {
       e.preventDefault();
       handleMenu();
       document.querySelector(e.target.getAttribute('href'))
         .scrollIntoView({ block: 'start', behavior: 'smooth' });
+      return;
     }
     if (e.target.parentNode === scrollBtn) {
       e.preventDefault();

121074277e19a19de2b02e198ae868a78c1ca03d
Vladimir Tkachenko <geletar@gmail.com>
2023-02-28 16:46:12 +0400

    Доработки по видеоуроку.

--- src/modules/modal.js
+++ src/modules/modal.js
@@ -1,7 +1,6 @@
 const modal = () => {
   const buttons = document.querySelectorAll('.popup-btn');
   const modal = document.querySelector('.popup');
-  const closeBtn = modal.querySelector('.popup-close');
   const modalContent = modal.querySelector('.popup-content');
 
   let offset = 0;
@@ -59,7 +58,11 @@ const modal = () => {
     button.addEventListener('click', openModal);
   });
 
-  closeBtn.addEventListener('click', closeModal);
+  modal.addEventListener('click', (e) => {
+    if (!e.target.closest('.popup-content') || e.target.classList.contains('popup-close')) {
+      closeModal();
+    }
+  });
 };
 
 export default modal;

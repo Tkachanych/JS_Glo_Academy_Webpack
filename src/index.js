ef1b46363dfe238ea9114b51644b56a8a1bc451c
Vladimir Tkachenko <geletar@gmail.com>
2023-03-01 10:04:58 +0400

    lesson23

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import toggleMenu from './modules/menu';
 import modal from './modules/modal';
 import calc from './modules/calc';
 import tabs from './modules/tabs';
+import slider from './modules/slider';
 
 timer('05 march 2023');
 toggleMenu();
 modal();
 calc();
 tabs();
+slider();

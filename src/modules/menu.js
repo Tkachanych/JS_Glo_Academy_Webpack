a1ac0227737c03d5303a222edeb1e2c82d37c72c
Vladimir Tkachenko <geletar@gmail.com>
2023-03-01 20:12:29 +0400

    Рефакторинг от ментора.

--- src/modules/menu.js
+++ src/modules/menu.js
@@ -1,31 +1,23 @@
 const toggleMenu = () => {
   const menu = document.querySelector('menu');
-  const body = document.querySelector('body');
-  const scrollBtn = body.querySelector('a');
+  const { body } = document;
 
   const handleMenu = () => {
     menu.classList.toggle('active-menu');
   };
 
+  const scrollToElem = (id) => {
+    document.getElementById(id)?.scrollIntoView({ block: 'start', behavior: 'smooth' });
+  };
+
   body.addEventListener('click', (e) => {
-    if (e.target.parentNode === scrollBtn) {
-      e.preventDefault();
-      document.querySelector(e.target.parentNode.getAttribute('href'))
-        .scrollIntoView({ block: 'start', behavior: 'smooth' });
-    }
-    if (e.target.closest('.active-menu')
-    && !e.target.classList.contains('active-menu')) {
-      e.preventDefault();
-      handleMenu();
-      document.querySelector(e.target.getAttribute('href'))
-        .scrollIntoView({ block: 'start', behavior: 'smooth' });
-      return;
-    }
-    if ((e.target.closest('.menu') && !e.target.closest('.active-menu'))
-      || e.target.classList.contains('close-btn')
-      || (e.target !== menu && menu.classList.contains('active-menu'))) {
-      handleMenu();
-    }
+    const { target } = e;
+    const isOpen = menu.classList.contains('active-menu');
+    const id = target.closest('[href="#service-block"], menu a')?.getAttribute('href').slice(1);
+
+    id && e.preventDefault();
+    (id && id !== 'close') && scrollToElem(id);
+    (target.closest('.menu') || (isOpen && (id || target !== menu))) && handleMenu();
   });
 };
 
